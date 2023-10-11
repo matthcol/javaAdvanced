6b3d5d945d4762b340f4537629f2404cf2ca810e
matthcol <matthias.colin@gmail.com>
2023-10-11 09:32:28 +0200

    instanceof

--- Geometry/src/test/java/data/demo/DemoForms.java
+++ Geometry/src/test/java/data/demo/DemoForms.java
@@ -1,12 +1,10 @@
 package data.demo;
 
-import data.Circle;
-import data.Form;
-import data.Point;
-import data.WeightedPoint;
+import data.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -54,7 +52,43 @@ class DemoForms {
 
     @Test
     void computeTotalSurface(){
+        var totalSurface = forms.stream()
+                .filter(f -> f instanceof Mesurable2D)
+                .map(f -> (Mesurable2D) f)
+                .mapToDouble(Mesurable2D::surface)
+                .sum();
+        System.out.println(totalSurface);
+    }
 
+    @Test
+    void demoSwitchCaseBefore(){
+        for (var form: forms){
+            if (form instanceof WeightedPoint){
+                WeightedPoint wp = (WeightedPoint) form;
+                System.out.println(wp.getWeight());
+            } else if (form instanceof  Point) {
+                Point pt = (Point) form;
+                System.out.println(pt.getX() + ", " + pt.getY());
+            } else if (form instanceof Mesurable2D){
+                Mesurable2D m = (Mesurable2D) form;
+                System.out.println(m.perimeter() + "," + m.surface());
+            }
+        }
+    }
+
+    @Test
+    void demoModernIfInstanceOf(){
+        for (var form: forms){
+            // modern form of instance of + cast new variable
+            if (form instanceof Point pt) {
+                System.out.println("Point: " + pt.getX() + ", " + pt.getY());
+            }
+            // NB:
+            if (form.getClass() == Point.class) {
+                Point pt = (Point) form;
+                System.out.println("Point (created as): " + pt.getX() + ", " + pt.getY());
+            }
+        }
     }
 
 }
